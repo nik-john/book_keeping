d67d04c1d26b59d752b7237292b9b6f9c25a805d
Nikhil John <nikhil.john.88@gmail.com>
2018-03-21 06:08:43 +0530

    Finished implementation

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const KEY = 'asddfas';
+const KEY = 'sssasdaawwswwww';
 
 var app = angular.module('app', []);
 
@@ -20,13 +20,14 @@ app.controller('main', ['$scope', '$http', '$interval', function ($scope, $http,
     var apiBase = 'https://bokiotestbankapi.azurewebsites.net/api/' + $scope.vd.apiKey;
 
     var init = function () {
-        $interval(() => loadTransactions(), 2000);
+        $interval(() => loadTransactions(), 5000);
     };
 
     var loadTransactions = function () {
         $http.get(apiBase + '/Transactions').then(function (response) {
             //Example API call. Most other calls are posts but how they are done can be seen in the documentation https://docs.angularjs.org/api/ng/service/$http 
             $scope.vd.transactionList = response.data; //this is  what we do on success. Error is not handled here.
+            console.log('Number of rows', $scope.vd.transactionList.length);
         });
     };
 
@@ -40,33 +41,49 @@ app.controller('main', ['$scope', '$http', '$interval', function ($scope, $http,
         });
     };
 
+    loadTransactions();
     init(); //Run init here to make sure things we call from init is defined when we call them
 }]);
 
+/* End Angular Code */
+/* Start  Implementation */
+let deDuped = false;
 function submitDataForParsing() {
     const text = document.getElementById('text').value;
+    if (!text.length) {
+        flashMessage('error', 'Please Paste your Bank Data First');
+        return false;
+    }
     const data = {
         text
     };
-    parseData(data);
+    return parseData(data);
 }
 
 function parseData(data) {
-    post(`https://bokiotestbankapi.azurewebsites.net/api/${KEY}/Transactions/Parse`, data).then(handleParsedData, error);
+    return post(`https://bokiotestbankapi.azurewebsites.net/api/${KEY}/Transactions/Parse`, data).then(handleParsedData, error);
 }
 
 function handleParsedData(data) {
-    createRawTable(data);
+    return createRawTable(data);
 }
 
 function createRawTable(data) {
     // Check for existence
+    if (!data.suggestions || !data.suggestions.length) {
+        flashMessage('error', 'Whoops! Something is wrong with this Data!');
+        return false;
+    }
     const tableNode = `
     <div class="tbl-header">
         <table cellpadding="0" cellspacing="0" border="0">
             <thead>
                 <tr>
                     ${data.suggestions.map(suggestion => {
+        if (!suggestion.options || !suggestion.options.length) {
+            flashMessage('error', 'Whoops! Something is wrong with this Data!');
+            return;
+        }
         return `<th>
                                     <select name="text">
                                         ${suggestion.options.map(option => {
@@ -85,7 +102,7 @@ function createRawTable(data) {
         <table cellpadding="0" cellspacing="0" border="0">
             <tbody>
                 ${data.rows.map(row => {
-        return `<tr>
+        return `<tr onclick="rowClickHandler()">
                     ${row.cells.map(cell => {
             return `<td>${cell.cleanedValue}</td>`;
         }).join('')}
@@ -96,42 +113,14 @@ function createRawTable(data) {
     </div>`;
     document.getElementById('raw-data').value = data;
     document.getElementById('parsed-table-container').innerHTML = tableNode;
+    flashMessage('success', 'Thank you. Click Save to proceed.');
 }
 
-// function createPreviewTable(data) {
-//     // Check for existence
-//     // Change table headers to dynamic
-//     const tableNode = `
-//     <div class="tbl-header">
-//         <table cellpadding="0" cellspacing="0" border="0">
-//             <thead>
-//                 <tr>
-//                     <th>Date</th>
-//                     <th>Text</th>
-//                     <th>Amount</th>
-//                 </tr>
-//             </thead>
-//         </table>
-//     </div>
-//     <div class="tbl-content">
-//         <table cellpadding="0" cellspacing="0" border="0">
-//             <tbody>
-//                 ${data
-//             .filter(row => !row.ignore)
-//             .map(row => {
-//                 return `<tr>           
-//                             <td>${row.transaction.date}</td>
-//                             <td>${row.transaction.text}</td>
-//                             <td>${row.transaction.amount}</td>
-//                         </tr>`;
-//             }).join('')}
-//             </tbody>
-//         </table>
-//     </div>`;
-//     document.getElementById('parsed-table-container').innerHTML = tableNode;
-// }
-
 function parseTableRowsToTransactions(rows) {
+    if (![].slice.call(rows)[0].childNodes[2] || ![].slice.call(rows)[0].childNodes[3] || ![].slice.call(rows)[0].childNodes[4]) {
+        flashMessage('error', 'No Data to Save!');
+        return false;
+    }
     return [].slice.call(rows).map((row, id) => {
         return {
             id,
@@ -141,40 +130,87 @@ function parseTableRowsToTransactions(rows) {
         };
     });
 }
+
 async function deDupe() {
     const tableRows = document.querySelectorAll('#parsed-table-container .tbl-content tr');
+    if (!tableRows.length) {
+        flashMessage('error', 'No Data to Save!');
+        return false;
+    }
+    const transactions = parseTableRowsToTransactions(tableRows);
+    if (!transactions) {
+        flashMessage('error', 'No Data to Save!');
+        return false;
+    }
     const preppedData = {
-        transactions: parseTableRowsToTransactions(tableRows)
+        transactions
     };
-    return post(`https://bokiotestbankapi.azurewebsites.net/api/${KEY}/Transactions/Preview`, preppedData).then(handlePreview, error);
+    return post(`https://bokiotestbankapi.azurewebsites.net/api/${KEY}/Transactions/Preview`, preppedData).then(handleDeDupe, error);
 }
-function handlePreview(data) {
-    document.getElementById('parsed-data').value = JSON.stringify(data);
-    data.filter(row => row.ignore).map(row => {
+
+function handleDeDupe(data) {
+    const parsedData = data.filter(row => !row.ignore);
+    const ignoredRows = data.filter(row => row.ignore);
+    ignoredRows.map(row => {
         [].slice.call(document.querySelectorAll('#parsed-table-container .tbl-content tr')).map((tableRow, index) => {
             if (index === row.transaction.id) {
                 document.querySelectorAll('#parsed-table-container .tbl-content tr')[index].classList.add('dupe');
             }
         });
     });
-    // createPreviewTable(data);
-}
-function handleSave(data) {
-    console.log("Saved");
-}
-function error(e) {
-    console.log("Error", e);
-    return;
-}
-async function saveData() {
-    // First deDupe
-    await deDupe();
-    const parsedData = JSON.parse(document.getElementById('parsed-data').value);
-    const preppedData = parsedData.map(row => row.transaction);
-    await post(`https://bokiotestbankapi.azurewebsites.net/api/${KEY}/Transactions`, preppedData).then(handleSave, error);
+    document.getElementById('parsed-data').value = JSON.stringify(parsedData);
+    document.getElementById('ignored-data').value = JSON.stringify(ignoredRows);
+    deDuped = true;
+    flashMessage('success', 'Checked for Duplicates. Click again to Save');
 }
 
-function post(url, body) {
+function addDuplicateRow(index) {
+    const ignoredRows = JSON.parse(document.getElementById('ignored-data').value);
+    let parsedRows = JSON.parse(document.getElementById('parsed-data').value);
+    let duplicateRow;
+    duplicateRow = [].slice.call(ignoredRows).filter(row => row.transaction.id === index)[0];
+    parsedRows = [...[].slice.call(parsedRows), duplicateRow];
+    const updatedIgnoredRows = [].slice.call(ignoredRows).filter(row => row.transaction.id !== index);
+    document.getElementById('parsed-data').value = JSON.stringify(parsedRows);
+    document.getElementById('ignored-data').value = JSON.stringify(ignoredRows);
+}
+
+function handleSave(data) {
+    flashMessage('success', 'Saved to Profile');
+}
+
+function error(e) {
+    flashMessage('error', 'Whoops! Something\'s Wrong!');
+    return;
+}
+
+async function saveData() {
+    if (!deDuped) {
+        return await deDupe();
+    }
+    const parsedData = JSON.parse(document.getElementById('parsed-data').value);
+    if (!parseData.length) {
+        flashMessage('error', 'No data to Save!');
+        return false;
+    }
+    const preppedData = parsedData.map(row => row.transaction);
+    // First deDupe
+    await post(`https://bokiotestbankapi.azurewebsites.net/api/${KEY}/Transactions`, preppedData).then(handleSave, error);
+    clearFields();
+    deDupe = false;
+}
+
+function clearFields() {
+    document.getElementById('text').value = '';
+    document.querySelector('.data-process .tbl-header thead').innerHTML = `<tr>
+            <th>Date</th>
+            <th>Description</th>
+            <th>Amount</th>
+        </tr>`;
+    document.querySelector('.data-process .tbl-content tbody').innerHTML = '<tr></tr>';
+}
+
+async function post(url, body) {
     return new Promise(function (resolve, reject) {
         const request = new XMLHttpRequest();
         request.onload = function () {
@@ -193,28 +229,59 @@ function post(url, body) {
     });
 }
 
-console.log(`/*  Features, Bugs and TODOs Etc. */
+function rowClickHandler() {
+    const rows = document.querySelectorAll('#parsed-table-container .tbl-content tr');
+    for (let i = 0; i < rows.length; i++) {
+        rows[i].addEventListener("click", e => {
+            addDuplicateRow(i);
+            rows[i].classList.remove('dupe');
+        });
+    }
+}
+/*  Displays self destruction Flash Messages to provide quick
+ *  User Feedback
+ */
+function flashMessage(type, message) {
+    document.querySelector('.flash').classList.add('showing');
+    document.querySelector('.flash .message').innerHTML = message;
+    document.querySelector('.flash .message').classList.add(type);
+    setTimeout(() => {
+        document.querySelector('.flash').classList.remove('showing');
+        document.querySelector('.flash .message').innerHTML = '';
+        document.querySelector('.flash .message').classList.remove(type);
+    }, 5000);
+}
+
+console.log(`NOTES
+    
     Features:
 
     • The user wants to see what they have saved before
     • The user wants to be able to save a new bank data paste
     • If Bokio doesn’t understand the paste perfectly the user might need to change the columns
     • If there are duplicates in the paste then the user might want to choose not to ignore the rows
-    Bokio thinks should be ignored.
+        Bokio thinks should be ignored.
 
-    Enhancements:
-    • Made the Angular App long poll for data, so that page reload can be avoided
+    Highlights/Enhancements:
+
+    • Application is completely Responsive and uses the lates and greatest CSS, including FlexBox & KeyFrames
     • Introduced SCSS
-    • Introduced ES6 using Babel
-    • 
-    • 
-    • 
-    • 
+    • Introduced ES6, ES7 using Babel
+    • Used async/await functions and native Promises
+    • Maximum functional programming (map, filter etc.)
+    • Reset CSS for cross browser consistency
+    • Instant feedback through Flash Messages
+    • No libraries used. Everything has been coded from scratch
+    • Tested for multiple data sets
+    • CSS animations on Buttons and Flash messages for Rich UX
+    • Error handling has been implemente for all major use cases
+    • Made the Angular App long poll for data, so that page reload can be avoided
+
 
     TODOs/Nice-to-haves:
-    • 
-    • 
-    • 
-    • 
+
+    • Commit column change to API
+    • Implement the Book Keeping on a Modal instead of block tables
+    • Use more Icons and lesser Text
 
     `);
--- appDev.js
+++ appDev.js
@@ -1,4 +1,4 @@
-const KEY = 'asddfas';
+const KEY = 'sssasdaawwswwww';
 
 var app = angular.module('app', []);
 
@@ -18,12 +18,13 @@ app.controller('main', ['$scope', '$http', '$interval', function ($scope, $http,
     var apiBase = 'https://bokiotestbankapi.azurewebsites.net/api/' + $scope.vd.apiKey;
 
     var init = function () {
-        $interval(() => loadTransactions(), 2000);
+        $interval(() => loadTransactions(), 5000);
     }
 
     var loadTransactions = function () {
         $http.get(apiBase + '/Transactions').then(function (response) { //Example API call. Most other calls are posts but how they are done can be seen in the documentation https://docs.angularjs.org/api/ng/service/$http 
             $scope.vd.transactionList = response.data; //this is  what we do on success. Error is not handled here.
+            console.log('Number of rows', $scope.vd.transactionList.length);
         });
     };
 
@@ -37,36 +38,50 @@ app.controller('main', ['$scope', '$http', '$interval', function ($scope, $http,
         });
     };
 
+    loadTransactions();
     init(); //Run init here to make sure things we call from init is defined when we call them
 }]);
 
 /* End Angular Code */
 /* Start  Implementation */
-function submitDataForParsing() {
-    const text = document.getElementById('text').value;
-    const data = {
-        text
-    };
-    return parseData(data);
-}
+    let deDuped = false;
+    function submitDataForParsing() {
+        const text = document.getElementById('text').value;
+        if(!text.length) {
+            flashMessage('error', 'Please Paste your Bank Data First');
+            return false;
+        }
+        const data = {
+            text
+        };
+        return parseData(data);
+    }
 
-function parseData(data) {
-    return post(`https://bokiotestbankapi.azurewebsites.net/api/${KEY}/Transactions/Parse`, data)
-        .then(handleParsedData, error);
-}
+    function parseData(data) {
+        return post(`https://bokiotestbankapi.azurewebsites.net/api/${KEY}/Transactions/Parse`, data)
+            .then(handleParsedData, error);
+    }
 
-function handleParsedData(data) {
-    createRawTable(data);
-}
+    function handleParsedData(data) {
+        return createRawTable(data);
+    }
 
-function createRawTable(data) {
-    // Check for existence
-    const tableNode = `
+    function createRawTable(data) {
+        // Check for existence
+        if(!data.suggestions || !data.suggestions.length) {
+            flashMessage('error', 'Whoops! Something is wrong with this Data!');
+            return false;
+        }
+        const tableNode = `
     <div class="tbl-header">
         <table cellpadding="0" cellspacing="0" border="0">
             <thead>
                 <tr>
                     ${data.suggestions.map(suggestion => {
+                        if(!suggestion.options || !suggestion.options.length) {
+                            flashMessage('error', 'Whoops! Something is wrong with this Data!');
+                            return;
+                        }
             return `<th>
                                     <select name="text">
                                         ${suggestion.options.map(option => {
@@ -85,7 +100,7 @@ function createRawTable(data) {
         <table cellpadding="0" cellspacing="0" border="0">
             <tbody>
                 ${data.rows.map(row => {
-            return `<tr>
+            return `<tr onclick="rowClickHandler()">
                     ${row.cells.map(cell => {
                     return `<td>${cell.cleanedValue}</td>`;
                 }).join('')}
@@ -94,137 +109,184 @@ function createRawTable(data) {
             </tbody>
         </table>
     </div>`;
-    document.getElementById('raw-data').value = data;
-    document.getElementById('parsed-table-container').innerHTML = tableNode;
-}
+        document.getElementById('raw-data').value = data;
+        document.getElementById('parsed-table-container').innerHTML = tableNode;
+        flashMessage('success', 'Thank you. Click Save to proceed.');
+    }
 
-// function createPreviewTable(data) {
-//     // Check for existence
-//     // Change table headers to dynamic
-//     const tableNode = `
-//     <div class="tbl-header">
-//         <table cellpadding="0" cellspacing="0" border="0">
-//             <thead>
-//                 <tr>
-//                     <th>Date</th>
-//                     <th>Text</th>
-//                     <th>Amount</th>
-//                 </tr>
-//             </thead>
-//         </table>
-//     </div>
-//     <div class="tbl-content">
-//         <table cellpadding="0" cellspacing="0" border="0">
-//             <tbody>
-//                 ${data
-//             .filter(row => !row.ignore)
-//             .map(row => {
-//                 return `<tr>           
-//                             <td>${row.transaction.date}</td>
-//                             <td>${row.transaction.text}</td>
-//                             <td>${row.transaction.amount}</td>
-//                         </tr>`;
-//             }).join('')}
-//             </tbody>
-//         </table>
-//     </div>`;
-//     document.getElementById('parsed-table-container').innerHTML = tableNode;
-// }
-
-function parseTableRowsToTransactions(rows) {
-    return [].slice.call(rows)
-        .map((row, id) => {
-            return {
-                id,
-                date: row.childNodes[2].innerHTML,
-                text: row.childNodes[3].innerHTML,
-                amount: row.childNodes[4].innerHTML.replace(' ', '').replace(',', '')
-            }
-        });
-}
-async function deDupe() {
-    const tableRows = document.querySelectorAll('#parsed-table-container .tbl-content tr');
-    const preppedData = {
-        transactions: parseTableRowsToTransactions(tableRows)
-    };
-    return post(`https://bokiotestbankapi.azurewebsites.net/api/${KEY}/Transactions/Preview`, preppedData)
-        .then(handlePreview, error);
-}
-function handlePreview(data) {
-    const parsedData = data.filter(row => row.ignore);
-    parsedData.map( row => {
-        [].slice.call(document.querySelectorAll('#parsed-table-container .tbl-content tr'))
-        .map((tableRow, index) => {
-            if(index === row.transaction.id) {
-                document.querySelectorAll('#parsed-table-container .tbl-content tr')[index].classList.add('dupe');
-            }
-        });
-    });
-    document.getElementById('parsed-data').value = JSON.stringify(parsedData);
-    // createPreviewTable(data);
-}
-function handleSave(data) {
-    console.log("Saved");
-}
-function error(e) {
-    console.log("Error", e);
-    return;
-}
-async function saveData() {
-    // First deDupe
-    await deDupe();
-    const parsedData = JSON.parse(document.getElementById('parsed-data').value);
-    const preppedData = parsedData.map(row => row.transaction);
-    await post(`https://bokiotestbankapi.azurewebsites.net/api/${KEY}/Transactions`, preppedData)
-        .then(handleSave, error);
-
-}
-
-function post(url, body) {
-    return new Promise(
-        function (resolve, reject) {
-            const request = new XMLHttpRequest();
-            request.onload = function () {
-                if (this.status === 200) {
-                    resolve(JSON.parse(this.response));
-                } else {
-                    reject(new Error(this.statusText));
+    function parseTableRowsToTransactions(rows) {
+        if (![].slice.call(rows)[0].childNodes[2] || ![].slice.call(rows)[0].childNodes[3] || ![].slice.call(rows)[0].childNodes[4]) {
+            flashMessage('error', 'No Data to Save!');
+            return false;
+        }
+        return [].slice.call(rows)
+            .map((row, id) => {
+                return {
+                    id,
+                    date: row.childNodes[2].innerHTML,
+                    text: row.childNodes[3].innerHTML,
+                    amount: row.childNodes[4].innerHTML.replace(' ', '').replace(',', '')
                 }
-            }
-            request.onerror = function () {
-                reject(new Error(
-                    'XMLHttpRequest Error: ' + this.statusText));
-            };
-            request.open('POST', url);
-            request.setRequestHeader('Content-Type', 'application/json');
-            request.send(JSON.stringify(body));
+            });
+    }
+
+    async function deDupe() {
+        const tableRows = document.querySelectorAll('#parsed-table-container .tbl-content tr');
+        if(!tableRows.length) {
+            flashMessage('error', 'No Data to Save!');
+            return false;
+        }
+        const transactions = parseTableRowsToTransactions(tableRows);
+        if(!transactions) {
+            flashMessage('error', 'No Data to Save!');
+            return false;
+        }
+        const preppedData = {
+            transactions
+        };
+        return post(`https://bokiotestbankapi.azurewebsites.net/api/${KEY}/Transactions/Preview`, preppedData)
+            .then(handleDeDupe, error);
+    }
+
+    function handleDeDupe(data) {
+        const parsedData = data.filter(row => !row.ignore);
+        const ignoredRows = data.filter(row => row.ignore);
+        ignoredRows.map(row => {
+            [].slice.call(document.querySelectorAll('#parsed-table-container .tbl-content tr'))
+                .map((tableRow, index) => {
+                    if (index === row.transaction.id) {
+                        document.querySelectorAll('#parsed-table-container .tbl-content tr')[index].classList.add('dupe');
+                    }
+                });
         });
-}
+        document.getElementById('parsed-data').value = JSON.stringify(parsedData);
+        document.getElementById('ignored-data').value = JSON.stringify(ignoredRows);
+        deDuped = true;
+        flashMessage('success', 'Checked for Duplicates. Click again to Save');
+    }
+
+    function addDuplicateRow(index) {
+        const ignoredRows = JSON.parse(document.getElementById('ignored-data').value);
+        let  parsedRows = JSON.parse(document.getElementById('parsed-data').value);
+        let duplicateRow;
+        duplicateRow = [].slice.call(ignoredRows).filter(row => row.transaction.id === index)[0];
+        parsedRows = [...[].slice.call(parsedRows), duplicateRow];
+        const updatedIgnoredRows = [].slice.call(ignoredRows).filter(row => row.transaction.id !== index);
+        document.getElementById('parsed-data').value = JSON.stringify(parsedRows);
+        document.getElementById('ignored-data').value = JSON.stringify(ignoredRows);
+    }
+
+    function handleSave(data) {
+        flashMessage('success', 'Saved to Profile');
+    }
+
+    function error(e) {
+        flashMessage('error', 'Whoops! Something\'s Wrong!');
+        return;
+    }
+
+    async function saveData() {
+        if(!deDuped) {
+            return await deDupe();
+        }
+        const parsedData = JSON.parse(document.getElementById('parsed-data').value);
+        if(!parseData.length) {
+            flashMessage('error', 'No data to Save!');
+            return false;
+        }
+        const preppedData = parsedData.map(row => row.transaction);
+        // First deDupe
+        await post(`https://bokiotestbankapi.azurewebsites.net/api/${KEY}/Transactions`, preppedData)
+            .then(handleSave, error);
+        clearFields();
+        deDupe = false;
+    }
+
+    function clearFields() {
+        document.getElementById('text').value = '';
+        document.querySelector('.data-process .tbl-header thead').innerHTML = `<tr>
+            <th>Date</th>
+            <th>Description</th>
+            <th>Amount</th>
+        </tr>`;
+        document.querySelector('.data-process .tbl-content tbody').innerHTML = '<tr></tr>';
+    }
+
+    async function post(url, body) {
+        return new Promise(
+            function (resolve, reject) {
+                const request = new XMLHttpRequest();
+                request.onload = function () {
+                    if (this.status === 200) {
+                        resolve(JSON.parse(this.response));
+                    } else {
+                        reject(new Error(this.statusText));
+                    }
+                }
+                request.onerror = function () {
+                    reject(new Error(
+                        'XMLHttpRequest Error: ' + this.statusText));
+                };
+                request.open('POST', url);
+                request.setRequestHeader('Content-Type', 'application/json');
+                request.send(JSON.stringify(body));
+            });
+    }
+
+    function rowClickHandler() {
+        const rows = document.querySelectorAll('#parsed-table-container .tbl-content tr');
+        for (let i = 0; i < rows.length; i++) {
+            rows[i].addEventListener("click", e => {
+                addDuplicateRow(i);
+                rows[i].classList.remove('dupe');
+            });
+        }
+    }
+    /*  Displays self destruction Flash Messages to provide quick
+     *  User Feedback
+     */
+    function flashMessage(type, message) {
+        document.querySelector('.flash').classList.add('showing');
+        document.querySelector('.flash .message').innerHTML = message;
+        document.querySelector('.flash .message').classList.add(type);
+        setTimeout(() => {
+            document.querySelector('.flash').classList.remove('showing');
+            document.querySelector('.flash .message').innerHTML = '';
+            document.querySelector('.flash .message').classList.remove(type);
+        }, 5000);
+    }
 
 console.log(
-    `/*  Features, Bugs and TODOs Etc. */
+    `NOTES
+    
     Features:
 
     • The user wants to see what they have saved before
     • The user wants to be able to save a new bank data paste
     • If Bokio doesn’t understand the paste perfectly the user might need to change the columns
     • If there are duplicates in the paste then the user might want to choose not to ignore the rows
-    Bokio thinks should be ignored.
+        Bokio thinks should be ignored.
 
-    Enhancements:
-    • Made the Angular App long poll for data, so that page reload can be avoided
+    Highlights/Enhancements:
+
+    • Application is completely Responsive and uses the lates and greatest CSS, including FlexBox & KeyFrames
     • Introduced SCSS
-    • Introduced ES6 using Babel
-    • 
-    • 
-    • 
-    • 
+    • Introduced ES6, ES7 using Babel
+    • Used async/await functions and native Promises
+    • Maximum functional programming (map, filter etc.)
+    • Reset CSS for cross browser consistency
+    • Instant feedback through Flash Messages
+    • No libraries used. Everything has been coded from scratch
+    • Tested for multiple data sets
+    • CSS animations on Buttons and Flash messages for Rich UX
+    • Error handling has been implemente for all major use cases
+    • Made the Angular App long poll for data, so that page reload can be avoided
+
 
     TODOs/Nice-to-haves:
-    • 
-    • 
-    • 
-    • 
 
-    `
-);
+    • Commit column change to API
+    • Implement the Book Keeping on a Modal instead of block tables
+    • Use more Icons and lesser Text
+
+    `);
